54c9da789121f416d9158862775d7109e5b61f56
Eduard Costenar <eduard.costenar@fortech.ro>
2020-12-18 11:39:22 +0200

    Run gradle test with junit

--- src/test/java/com/example/demo/dto/EmployeeTest.java
+++ src/test/java/com/example/demo/dto/EmployeeTest.java
@@ -1,7 +1,8 @@
 package com.example.demo.dto;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
 
 import java.io.IOException;
 import java.nio.file.Files;
