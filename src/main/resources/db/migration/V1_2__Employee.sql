04e13459546fac7e8dd104f63440285a96ce2f0e
Norbert Jambor <norbert.jambor@fortech.ro>
2020-12-22 10:01:10 +0200

    error fixed

--- src/main/resources/db/migration/V1_2__Employee.sql
+++ src/main/resources/db/migration/V1_2__Employee.sql
@@ -3,8 +3,7 @@ create table EMPLOYEE
     ID bigint not null constraint PK_EMPLOYEE primary key,
     JOB varchar (255),
     NAME varchar (10),
-    DEPARTMENT_ID bigint not null constraint FK_EMPLOYEE_DEPARTMENT_ID references DEPARTMENT(ID),
-    TEAM_ID bigint not null constraint FK_EMPLOYEE_TEAM_ID references TEAM(ID)
+    DEPARTMENT_ID bigint not null constraint FK_EMPLOYEE_DEPARTMENT_ID references DEPARTMENT(ID)
 );
 CREATE SEQUENCE S_EMPLOYEE
 START WITH 1
